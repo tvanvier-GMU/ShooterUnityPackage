c43151a7a6c47017b69c05128d799c90805a0de4
tvanvier-GMU <tvanvier@gmu.edu>
2020-03-07 11:34:02 -0500

    Add files via upload

---
+++ MoveBetween.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MoveBetween : MonoBehaviour
+{
+
+    public Transform pointA;
+    public Transform pointB;
+    public float speed = 1;
+    Transform currentTarget;
+    const float errorMargin = .001f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTarget = pointA;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(transform.position, currentTarget.position) < errorMargin)
+        {
+            if (currentTarget == pointA)
+                currentTarget = pointB;
+            else
+                currentTarget = pointA;
+        }
+        transform.position = Vector3.MoveTowards(transform.position, currentTarget.position, speed * Time.deltaTime);
+    }
+}
