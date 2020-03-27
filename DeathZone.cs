64a9fc67721dbc75430e30f5b614259e60869ab7
tvanvier-GMU <tvanvier@gmu.edu>
2020-03-27 15:00:43 -0400

    Add files via upload

---
+++ DeathZone.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//The idea of this script is that it will be used on a trigger area, to send you back to the starting position in your level.
+//it's a penalty that will waste your precious time!
+[RequireComponent(typeof(Collider))]
+public class DeathZone : MonoBehaviour
+{
+    public Transform respawnPoint;
+    public string playerTag = "Player";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag(playerTag))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
